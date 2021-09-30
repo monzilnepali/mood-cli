74a19feeeb232b1ed195363833f89906d3cd906a
monzilnepali <manjilnepali98@gmail.com>
2021-09-30 13:57:56 +0545

    Remove duplicate from constant

--- constants/constants.go
+++ constants/constants.go
@@ -1,10 +1,8 @@
 package constants
 
 var SoundData = map[string]string{
-	"birds":         "resources/birds.ogg",
-	"outdoor_birds": "resources/outdoor-birds.ogg",
-	"rain":          "resources/rain.ogg",
-	"waves":         "resources/waves.ogg",
+	"birds": "resources/birds.ogg",
+	"waves": "resources/waves.ogg",
 }
 
 var SoundList = []string{"birds", "waves"}
